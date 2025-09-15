715d8c532d84da1e9a956366a4d7f8a51ca891e7
fauxsaurus <31806063+fauxsaurus@users.noreply.github.com>
2025-09-15 16:54:34 -0500

    spunoff todo list

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -83,8 +83,6 @@ function App() {
 			.catch(console.error)
 
 		console.log(data)
-
-		/** @todo upload epub, unzip, get all text files, return list to the client, client: load each file (WITH proper CSS), query the iframe, draw all found text to images, replace the markup (including the alt text source) with new image markup using the .kindle-full-line-image images/file-img-#.ext format for the source, send images and the new XHTML off to the server, send new CSS to server, server (just add a new css file and link to it in every xhtml document to ensure the rules are applied everywhere where a query is matched--ignore files where the contents remain unchanged): add these files to a new zip and export the epub (with a validation step at the end?), send a download link to the client, client initiate an auto download (might not be able to do that without the click... gotta keep the onSubmit event open the whole time?) download name = file.name-pre-converted-accessible-kindle.epub */
 	}
 
 	return (
