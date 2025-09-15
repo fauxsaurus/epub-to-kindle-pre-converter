3d67e137ae34c13a623039e96390965acf54127b
fauxsaurus <31806063+fauxsaurus@users.noreply.github.com>
2025-09-15 16:29:39 -0500

    +HTTPS to client

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ePub to Kindle Pre-Converter
+
 Generates an epub that replaces screen-reader-only text and unsupported characters (i.e., Korean) with images and alt text.
+
+## Config
+
+Setup a `.env` file in the repo's root directory.
+
+```
+SSL_CERT_PATH=/file/path/cert.pem
+SLL_KEY_PATH=/file/path/key.pem
+```
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
+import dotenv from 'dotenv'
+import fs from 'fs'
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+
+dotenv.config()
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	root: 'client',
+	server: {
+		https: {
+			key: fs.readFileSync(process.env.SSL_KEY_PATH!),
+			cert: fs.readFileSync(process.env.SSL_CERT_PATH!),
+		},
+	},
 })
