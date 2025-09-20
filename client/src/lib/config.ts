0042488db239d4e41d9e071de3c0fa7a6396e7e2
fauxsaurus <31806063+fauxsaurus@users.noreply.github.com>
2025-09-20 12:51:49 -0500

    -tmp default config

--- client/src/lib/config.ts
+++ client/src/lib/config.ts
@@ -25,44 +25,11 @@ export type IConfig = {
 	}
 }
 
-const DEFAULT_PRE_CSS_RULES = `/** @note adjusts text *before* conversion for better image optimization */
-.braille,.runes,[lang="ko"] {
-	margin: 1rem auto;
-	font-size: 1.75rem;
-	text-align: center;
-}`
-
-const DEFAULT_POST_CSS_RULES = `/** @note Full-width image with one line above and below. */
-.kindle-accessible-image {
-	margin: 0 auto;
-	width: 100%;
-}
-`
-
 const info = `For use with https://github.com/fauxsaurus/epub-to-awz-kfx-pre-converter.`
 
 export const DEFAULT_CONFIG: IConfig = {
-	css: {
-		pre: DEFAULT_PRE_CSS_RULES,
-		post: DEFAULT_POST_CSS_RULES,
-	},
-	img: [
-		{
-			alt: '.braille+.screen-reader-only',
-			class: 'kindle-accessible-image',
-			content: '.braille',
-		},
-		{
-			alt: '.runes+.screen-reader-only',
-			class: 'kindle-accessible-image',
-			content: '.runes',
-		},
-		{
-			alt: '',
-			class: 'kindle-accessible-image',
-			content: '[lang="ko"]',
-		},
-	],
+	css: {pre: '', post: ''},
+	img: [],
 	meta: {info, version: 1},
 }
 
